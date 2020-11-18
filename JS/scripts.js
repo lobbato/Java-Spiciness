fb1fb82619a4378e97bbeaeec74e40246dfa553b
lobbato <emily.lobbato@gmail.com>
2020-11-17 18:14:11 -0800

    updated bug-a-lishous text

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
+  $("#formOne").submit(function() {
     const person1Input = $("input#person1").val();
     const person2Input = $("input#person2").val();
     const animalInput= $("input#animal").val();
@@ -7,15 +7,13 @@ $(document).ready(function() {
     const verbInput = $("input#verb").val();
     const nounInput = $("input#noun").val();
 
-    $(".person1").text(person1Input);
+    $("person1").text(person1Input);
     $(".person2").text(person2Input);
     $(".animal").text(animalInput);
     $(".exclamation").text(exclamationInput);
     $(".verb").text(verbInput);
     $(".noun").text(nounInput);
 
-    $("#story").show();
-
-    event.preventDefault();
+    $("#story").sho();
   });
 });
